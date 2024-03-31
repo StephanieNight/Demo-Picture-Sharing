5597844f16bc4582ebb31f8e9f7abd63f91f5c5a
Stephanie Night <s.hvenegaard@outlook.com>
2024-03-31 11:55:10 +0200

    added front end

---
+++ src/frontend/picture-sharing-app/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using picture_sharing.Models;
+using System.Diagnostics;
+
+namespace picture_sharing.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+        public HomeController(ILogger<HomeController> logger, IConfiguration config, HttpClient httpClient)
+        {
+            _logger = logger;
+            _configuration = config;
+            _httpClient = httpClient;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+
+            return View();           
+        }
+        public async Task<IActionResult> Gallery()
+        {
+            var url = _configuration.GetSection("BackendURL").Value;
+            var response = await _httpClient.GetAsync($"{url}/Gallery");
+            if (response.IsSuccessStatusCode)
+            {
+                var images = await response.Content.ReadFromJsonAsync<List<Uri>>();
+                return View(images);
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/frontend/picture-sharing-app/Program.cs
@@ -0,0 +1,42 @@
+using picture_sharing.Services;
+
+namespace picture_sharing
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddHttpClient();
+            // Replace <YourKeyVaultName> with your Key Vault name
+            var keyVaultUri = $"https://kvdevpictureshare.vault.azure.net";
+            builder.Services.AddSingleton(new KeyVaultService(keyVaultUri));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
